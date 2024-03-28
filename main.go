cc23cbcd039c0032f411229a87a857fcef61bf11
tangx <uyinn28@gmail.com>
2024-03-28 19:45:00 +0800

    [Issue #2] 使用 go template 创建一个简单页面 (#4)

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func main() {
 	// 健康检查接口
 	r.GET("/ping", pingHandler)
 
+	// #2. 使用 LoadHTMLGlob 加载模板文件
+	r.LoadHTMLGlob("tmpl/*")
+
+	// Login GET
+	r.GET("/login", getLoginHandler)
+
 	err := r.Run(":8080")
 	if err != nil {
 		panic(err)
@@ -22,4 +28,10 @@ func pingHandler(c *gin.Context) {
 	// 这里的 status 不建议直接使用 200，建议使用 http.StatusOK
 	// 语义化之后更加直观，不容易出错
 	c.String(http.StatusOK, "pong")
+}
+
+func getLoginHandler(c *gin.Context) {
+	// #2. 使用模版文件
+	// 完整页面， 没有数据传入
+	c.HTML(http.StatusOK, "login.tmpl", nil)
 }
