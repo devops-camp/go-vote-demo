f6b59a766e93ce348086965924410bb112544bd7
tangx <uyinn28@gmail.com>
2024-03-28 20:02:21 +0800

    [Issue #5] Post 提交表单

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ func main() {
 
 	// Login GET
 	r.GET("/login", getLoginHandler)
+	// #5. 获取表单数据
+	r.POST("/login", postLoginHandler)
 
 	err := r.Run(":8080")
 	if err != nil {
@@ -34,4 +36,24 @@ func getLoginHandler(c *gin.Context) {
 	// #2. 使用模版文件
 	// 完整页面， 没有数据传入
 	c.HTML(http.StatusOK, "login.tmpl", nil)
+}
+
+func postLoginHandler(c *gin.Context) {
+	// #5 获取表单数据
+	user := &User{}
+
+	err := c.ShouldBind(user)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		// 显示错误信息之后， 一定要 return 结束后序逻辑
+		return
+	}
+
+	// 成功后显示用户信息
+	c.JSON(http.StatusOK, user)
+}
+
+type User struct {
+	Name     string `form:"name" binding:"required" json:"name"`
+	Password string `form:"password" binding:"required" json:"password"`
 }
